990edc4f32d89dc436dd19dce5ec2481bca02e80
memesarecool <33184334+memesarecool@users.noreply.github.com>
2018-10-20 18:40:47 -0400

    Update file_creator.py

--- file_creator.py
+++ file_creator.py
@@ -4,7 +4,7 @@ import time
 BASIC_PATH = str(os.getcwd())
 CURRENT_TIME = str(time.strftime('[%d-%m-%Y %H-%M-%S]'))
 FOLDER_PATH = BASIC_PATH + '\\result\\' + CURRENT_TIME + '\\'
-txt_names = ['insecure', 'minecon', 'optifine', '5zig', 'labymod', 'special_name', 'working', 'hypixelRank', 'hypixelLevel', 'mineplexRank']
+txt_names = ['unsecure', 'minecon', 'optifine', '5zig', 'labymod', 'special_name', 'working', 'hypixelRank', 'hypixelLevel', 'mineplexRank']
 
 
 def create_files():
