4f43796e3e0ec2cfd142ff3fa2301e69de18b5c1
memesarecool <33184334+memesarecool@users.noreply.github.com>
2018-10-20 18:31:01 -0400

    Added Mineplex Rank Check to Output

--- file_creator.py
+++ file_creator.py
@@ -3,13 +3,13 @@ import time
 
 BASIC_PATH = str(os.getcwd())
 CURRENT_TIME = str(time.strftime('[%d-%m-%Y %H-%M-%S]'))
-FOLDER_PATH = BASIC_PATH + '\\' + CURRENT_TIME + '\\'
-txt_names = ['unsecure', 'minecon', 'optifine', '5zig', 'labymod', 'special_name', 'working']
+FOLDER_PATH = BASIC_PATH + '\\result\\' + CURRENT_TIME + '\\'
+txt_names = ['insecure', 'minecon', 'optifine', '5zig', 'labymod', 'special_name', 'working', 'hypixelRank', 'hypixelLevel', 'mineplexRank']
 
 
 def create_files():
     if not os.path.exists(FOLDER_PATH):
         os.makedirs(FOLDER_PATH)
-    for x in range(len(txt_names) -1):
+    for x in range(len(txt_names)):
         if not os.path.exists(FOLDER_PATH + txt_names[x] + '.txt'):
             open(FOLDER_PATH + txt_names[x] + '.txt', 'a').close()
