78f77a9d43ffff57d7ed7f9b3ed5df475b69211f
memesarecool <33184334+memesarecool@users.noreply.github.com>
2018-10-20 18:32:22 -0400

    Added Mineplex Rank Check & Fixed Hypixel Rank Check
    Added Mineplex Rank Check & Optimizied Hypixel Rank Check

--- special_checker.py
+++ special_checker.py
@@ -7,30 +7,30 @@ def under_four_character_long(USERNAME):
     else:
         return False
 def hypixel_rank_check(USERNAME):
-    try:
-        request_response = requests.get('https://plancke.io/hypixel/player/stats/' + USERNAME).content
-        if (str(request_response).__contains__("Player not found")): return False
-        if str(request_response).__contains__("OWNER"): return 'Owner'
-        elif str(request_response).__contains__("ADMIN"): return 'Admin'
-        elif str(request_response).__contains__("MOD"): return 'Moderator'
-        elif str(request_response).__contains__("HELPER"): return 'Helper'
-        elif str(request_response).__contains__("BUILD TEAM"): return 'Builder'
-        elif str(request_response).__contains__("YOUTUBE"): return 'YouTube'
-        elif str(request_response).__contains__("MVP++"): return 'MVP++'
-        elif str(request_response).__contains__("MVP+"): return 'MVP+'
-        elif str(request_response).__contains__("MVP"): return 'MVP'
-        elif str(request_response).__contains__("VIP+"): return 'VIP+'
-        elif str(request_response).__contains__("VIP"): return 'VIP'
-        else: return False
-    except Exception as e:
-        print(e)
-        pass
+	response = str(requests.get('https://plancke.io/hypixel/player/stats/' + USERNAME).text)
+	if response.__contains__("That player cannot be found."): return False
+	else:
+		rank = re.search("\"\[.*?\]", response)
+		if(bool(rank) != False):
+			match = re.search("\"\[.*?\]", response).group().split("[")[1].split("]")[0]
+			return match
+		return False
+print (hypixel_rank_check("stevenr8"))
 def hypixel_level_check(USERNAME):
     request_response = requests.get('https://plancke.io/hypixel/player/stats/' + USERNAME).content
-    if (str(request_response).__contains__("Player not found")): return False
-    match = re.search("Level:<\/b> .*?Karma", str(request_response)).group()
-    stepTwoOfTrashCode = match.split("<")
-    okHereYouGo = stepTwoOfTrashCode[1].split('>')
-    return okHereYouGo[1]
-    
+    if (str(request_response).__contains__("Player not found")):
+        return False
+    else:
+        match = re.search("<b>Level:</b> [1-9]*[^><]*[1-9]*", str(request_response)).group()
+        level = match.split("</b> ")
+		
+        return level[1]
+def mineplex_rank_check(USERNAME):
+	response = str(requests.get('https://www.mineplex.com/players/' + USERNAME).text)
+	if response.__contains__("That player cannot be found."): return False
+	else:
+		match = re.search("Rank\(\'.*?\'\)", response).group().split("('")[1].split("')")
+		if (match[0] == "PLAYER"): return False 
+		return match[0]
+
 #OG-Account check planned
