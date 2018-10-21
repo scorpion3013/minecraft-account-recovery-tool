02e38e37b874a887e2102643ba46687491f8f7c0
scorpion3013 <coc909scorp@gmail.com>
2018-10-21 10:51:31 +0200

    Improved the mineplex_rank_check regex. Made the code more PEP8 conform.

--- special_checker.py
+++ special_checker.py
@@ -8,16 +8,19 @@ def under_four_character_long(USERNAME):
     else:
         return False
 
+
 def hypixel_rank_check(USERNAME):
 
-	response = str(requests.get('https://plancke.io/hypixel/player/stats/' + USERNAME).text)
-	if response.__contains__("Player not found"): return False
-	else:
-		rank = re.search("\"\[.*?\]", response)
-		if(bool(rank) != False):
-			match = re.search("\"\[.*?\]", response).group().split("[")[1].split("]")[0]
-			return match
-		return False
+    response = str(requests.get('https://plancke.io/hypixel/player/stats/' + USERNAME).text)
+    if response.__contains__("Player not found"):
+        return False
+    else:
+        rank = re.search("\"\[.*?\]", response)
+        if(bool(rank) != False):
+            match = re.search("\"\[.*?\]", response).group().split("[")[1].split("]")[0]
+            return match
+        return False
+
 
 def hypixel_level_check(USERNAME):
 
@@ -29,13 +32,16 @@ def hypixel_level_check(USERNAME):
         level = match.split("</b> ")
         return level[1]
 
+
 def mineplex_rank_check(USERNAME):
 
-	response = str(requests.get('https://www.mineplex.com/players/' + USERNAME).text)
-	if response.__contains__("That player cannot be found."): return False
-	else:
-		match = re.search("Rank\(\'.*?\'\)", response).group().split("('")[1].split("')")
-		if (match[0] == "PLAYER"): return False 
-		return match[0]
+    response = str(requests.get('https://www.mineplex.com/players/' + USERNAME).text)
+    if response.__contains__("That player cannot be found."):
+        return False
+    else:
+        match = "{group}".format(group=re.search(r"Rank\(\'(.*)\'\)", response).group(1))
+        if (match == "player"):
+            return False
+        else:
+            return match
   
-#OG-Account check planned
