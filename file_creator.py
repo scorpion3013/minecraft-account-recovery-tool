9ce8811068541277f3dbcce18b16db60d3987b9f
scorpion3013 <coc909scorp@gmail.com>
2018-10-21 16:19:47 +0200

    replaced \\ with  os.sep to make it linux compatible

--- file_creator.py
+++ file_creator.py
@@ -3,8 +3,9 @@ import time
 
 BASIC_PATH = str(os.getcwd())
 CURRENT_TIME = str(time.strftime('[%d-%m-%Y %H-%M-%S]'))
-FOLDER_PATH = BASIC_PATH + '\\result\\' + CURRENT_TIME + '\\'
-txt_names = ['unsecure', 'minecon', 'optifine', '5zig', 'labymod', 'special_name', 'working', 'hypixelRank', 'hypixelLevel', 'mineplexRank']
+FOLDER_PATH = BASIC_PATH + os.sep + 'result' + os.sep + CURRENT_TIME + os.sep
+txt_names = ['unsecure', 'minecon', 'optifine', '5zig', 'labymod', 'special_name', 'working', 'hypixelRank',
+             'hypixelLevel', 'mineplexRank']
 
 
 def create_files():
--- main.py
+++ main.py
@@ -35,50 +35,50 @@ for x in range(len(account_file_lines)):
 
         mpRank = mineplex_rank_check(USERNAME)
 
-        open(FOLDER_PATH + '\\working.txt', 'a').write(account_file_lines[x] + "\n")
+        open(FOLDER_PATH + os.sep + 'working.txt', 'a').write(account_file_lines[x] + "\n")
         Counter.valid += 1
 
         if bool(answer["user"]["secured"]) is False:
-            open(FOLDER_PATH + '\\unsecure.txt', 'a').write(account_file_lines[x] + "\n")
+            open(FOLDER_PATH + os.sep + 'unsecure.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.insecure += 1
             unsecure = USERNAME + ' is unsecure\n'
         if minecon_cape_request(UUID) is True:
-            open(FOLDER_PATH + '\\minecon.txt', 'a').write(account_file_lines[x] + "\n")
+            open(FOLDER_PATH + os.sep + 'minecon.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.minecon += 1
             minecon = USERNAME + ' has a minecon cape\n'
         if five_zig_cape_request(UUID) is True:
-            open(FOLDER_PATH + '\\5zig.txt', 'a').write(account_file_lines[x] + "\n")
+            open(FOLDER_PATH + os.sep + '5zig.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.fivezig += 1
             fivezig = USERNAME + ' has a 5zig cape\n'
         if optifine_cape_request(USERNAME) is True:
-            open(FOLDER_PATH + '\\optifine.txt', 'a').write(account_file_lines[x] + "\n")
+            open(FOLDER_PATH + os.sep + 'optifine.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.optifine += 1
             optifine = USERNAME + ' has a optifine cape\n'
         if laby_mod_cape_request(UUID) is True:
-            open(FOLDER_PATH + '\\labymod.txt', 'a').write(account_file_lines[x] + "\n")
+            open(FOLDER_PATH + os.sep + 'labymod.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.labymod += 1
             labymod = USERNAME + ' has a labymod cape\n'
         if liquidbounce_cape_request(UUID) is True:
-            open(FOLDER_PATH + '\\liquidbounce.txt', 'a').write(account_file_lines[x] + "\n")
+            open(FOLDER_PATH + os.sep + 'liquidbounce.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.liquidbounce += 1
             liquidbounce = USERNAME + ' has a liquidbounce cape\n'
 
         if hp[0] != 'False':
-            open(FOLDER_PATH + '\\hypixelRank.txt', 'a').write(account_file_lines[x] + ' Rank: ' + hp[0] + "\n")
+            open(FOLDER_PATH + os.sep + 'hypixelRank.txt', 'a').write(account_file_lines[x] + ' Rank: ' + hp[0] + "\n")
             Counter.hypixelrank += 1
             hypixelrank = USERNAME + ' Hypixelrank: ' + hp[0] + '\n'
 
         if int(hp[1]) >= hypixel_min_level and hp[1] != 0:
-            open(FOLDER_PATH + '\\hypixelLevel.txt', 'a').write(account_file_lines[x] + ' Level: ' + str(hp[1]) + "\n")
+            open(FOLDER_PATH + os.sep + 'hypixelLevel.txt', 'a').write(account_file_lines[x] + ' Level: ' + str(hp[1]) + "\n")
             Counter.hypixellevel += 1
             hypixellevel = USERNAME + ' Hypixellevel: ' + str(hp[1]) + '\n'
 
         if mpRank is not False:
-            open(FOLDER_PATH + '\\mineplexRank.txt', 'a').write(account_file_lines[x] + ' Rank: ' + mpRank + "\n")
+            open(FOLDER_PATH + os.sep + 'mineplexRank.txt', 'a').write(account_file_lines[x] + ' Rank: ' + mpRank + "\n")
             Counter.mineplexrank += 1
             mineplexrank = USERNAME + ' Mineplexrank: ' + mpRank + '\n'
         if under_four_character_long(USERNAME) is True:
-            open(FOLDER_PATH + '\\special_name.txt', 'a').write(account_file_lines[x] + "\n")
+            open(FOLDER_PATH + os.sep + 'special_name.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.shortname += 1
             shortname = USERNAME + ' has a short name\n'
         print('Valid account ' + USERNAME + '\n' + unsecure + minecon + fivezig + optifine + labymod + liquidbounce
