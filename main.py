b28d78b3387e2cf21a376a9405c5ffb2b7ba34ef
scorpion3013 <coc909scorp@gmail.com>
2018-12-12 12:37:35 +0100

    Implemented the config more Changed the console print

--- main.py
+++ main.py
@@ -34,73 +34,63 @@ def check(x):
     if not str(answer).__contains__('Invalid credentials'):
         uuid = answer["availableProfiles"][0]["id"]
         username = answer["availableProfiles"][0]["name"]
-        unsecure = minecon = fivezig = optifine = \
-            labymod = liquidbounce = hypixelrank = \
-            hypixellevel = mineplexrank = shortname = ''
         open(FOLDER_PATH + os.sep + 'working.txt', 'a').write(account_file_lines[x] + "\n")
         Counter.valid += 1
 
         if Checker.Level.hypixel_level or Checker.Rank.hypixel_rank:
-            hp = hypixel_check_api(username)
+            if Checker.Hypixel.method == 0:
+                hp = hypixel_check_api(username)
+            else:
+                hp = hypixel_check_plank(username)
+
             if Checker.Rank.hypixel_rank:
                 if hp[0] != 'False':
                     open(FOLDER_PATH + os.sep + 'hypixelRank.txt', 'a').write(
                         account_file_lines[x] + ' Rank: ' + hp[0] + "\n")
                     Counter.hypixelrank += 1
-                    hypixelrank = '\n' + ' Hypixelrank: ' + hp[0]
             if Checker.Level.hypixel_level:
                 if int(hp[1]) >= Checker.Level.hypixel_min_level and hp[1] != 0:
                     open(FOLDER_PATH + os.sep + 'hypixelLevel.txt', 'a').write(
                         account_file_lines[x] + ' Level: ' + str(hp[1]) + "\n")
                     Counter.hypixellevel += 1
-                    hypixellevel = '\n' + ' Hypixellevel: ' + str(hp[1])
         if Checker.Rank.mineplex_rank:
             mp_rank = mineplex_rank_check(username)
             if mp_rank is not False:
                 open(FOLDER_PATH + os.sep + 'mineplexRank.txt', 'a').write(
                     account_file_lines[x] + ' Rank: ' + str(mp_rank) + "\n")
                 Counter.mineplexrank += 1
-                mineplexrank = '\n' + ' Mineplexrank: ' + str(mp_rank)
         if bool(answer["user"]["secured"]) is False:
             open(FOLDER_PATH + os.sep + 'unsecure.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.insecure += 1
-            unsecure = '\n' + 'Unsecure'
         if Checker.Cape.minecon:
             if minecon_cape_request(uuid) is True:
                 open(FOLDER_PATH + os.sep + 'minecon.txt', 'a').write(account_file_lines[x] + "\n")
                 Counter.minecon += 1
-                minecon = '\n' + ' Minecon cape'
         if Checker.Cape.fivezig:
             if five_zig_cape_request(uuid) is True:
                 open(FOLDER_PATH + os.sep + '5zig.txt', 'a').write(account_file_lines[x] + "\n")
                 Counter.fivezig += 1
-                fivezig = '\n' + ' 5zig cape'
         if Checker.Cape.optifine:
             if optifine_cape_request(username) is True:
                 open(FOLDER_PATH + os.sep + 'optifine.txt', 'a').write(account_file_lines[x] + "\n")
                 Counter.optifine += 1
-                optifine = '\n' + ' optifine cape'
         if Checker.Cape.labymod:
             if laby_mod_cape_request(uuid) is True:
                 open(FOLDER_PATH + os.sep + 'labymod.txt', 'a').write(account_file_lines[x] + "\n")
                 Counter.labymod += 1
-                labymod = '\n' + ' labymod cape'
         if Checker.Cape.liquidbounce:
             if liquidbounce_cape_request(uuid) is True:
                 open(FOLDER_PATH + os.sep + 'liquidbounce.txt', 'a').write(account_file_lines[x] + "\n")
                 Counter.liquidbounce += 1
-                liquidbounce = '\n' + ' LiquidBounce cape'
         if under_four_character_long(username) is True:
             open(FOLDER_PATH + os.sep + 'special_name.txt', 'a').write(account_file_lines[x] + "\n")
             Counter.shortname += 1
-            shortname = '\n' + ' Short name'
 
-        print('Valid account ' + username + '\n' + unsecure + minecon + fivezig + optifine + labymod + liquidbounce
-              + hypixelrank + hypixellevel + mineplexrank + shortname
-              + '\n' + '---' * 6 + '\n')
+        print("Valid account " + username)
     else:
-        print('Invalid Account' + '\n' + '---' * 6 + '\n')
+        print('Invalid Account')
         Counter.invalid += 1
+
 
 def theads_two(numbers, threads=1):
     pool = ThreadPool(threads)
@@ -108,6 +98,7 @@ def theads_two(numbers, threads=1):
     pool.close()
     pool.join()
     return results
+
 
 if __name__ == "__main__":
     countt = []
@@ -123,7 +114,7 @@ Counter_list = [str(Counter.valid) + ' Valid accounts',str(Counter.invalid) + ' 
              str(Counter.hypixelrank) + ' Hypixel-Rank accounts',str(Counter.hypixellevel) + ' Hypixel-Level accounts',
              str(Counter.mineplexrank) + ' Mineplex-Rank accounts',str(Counter.shortname) + ' Short-name accounts']
 
-print('\n\nResult:\n')
+print('\nResult:\n')
 
 for x in range(len(Counter_list) - 0):
     print(Counter_list[x])
