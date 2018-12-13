9e577369717b591911e30334028177f5bc3e5145
scorpion3013 <coc909scorp@gmail.com>
2018-12-13 14:24:23 +0100

    SomeStuff

--- stuff/checker.py
+++ stuff/checker.py
@@ -1,5 +1,6 @@
 import json
 import os
+import platform
 import random
 import time
 from multiprocessing.dummy import Pool as ThreadPool
@@ -14,6 +15,8 @@ colorama.init()
 
 class proxy:
     proxys = []
+    invalid = 0
+    working = []
 
 def proxy_getter():
         if Checker.Proxy.proxy:
@@ -40,51 +43,57 @@ def account_login(email_username, password):
        else:
            while True:
 
-               proxyy = str(random.choice(proxy.proxys))
+               proxyy = proxy.proxys[random.randrange(0, len(proxy.proxys), 1)]
                proxy_dict = {
                    'http': "http://" + proxyy.replace("\n", ""),
-                   'https': "http://" + proxyy.replace("\n", "")
+                   'https': "https://" + proxyy.replace("\n", "")
                }
                try:
-                   answer = requests.post('https://authserver.mojang.com/authenticate', data=request_body, headers=headers, proxies=proxy_dict, timeout=10).content
+                   answer = requests.post('https://authserver.mojang.com/authenticate', data=request_body, headers=headers, proxies=proxy_dict, timeout=15).content
                    break
                except Exception as e:
-                   #print(e)
-                   proxy.proxys.remove(proxyy)
-                   print("\nProxy removed cause it was shit.")
+                   #proxy.proxys.remove(proxyy)
+                   print("\nInvalid request, repeating.")
                    time.sleep(2)
                    continue
 
        answer_json = json.loads(answer)
    except Exception as e:
-       #print(e)
        answer_json = 'Invalid credentials'
    return answer_json
 
 def checkproxies(threads, timeout, proxyjudge):
     myip = str(requests.get("https://api.ipify.org/").content.decode())
     proxies = open(BASIC_PATH + "/proxies.txt").readlines()
-    working = []
+    windows = False
 
+    if platform.system() == "Windows":
+        import ctypes
+        windows = True
     def proxy_checker(x):
         proxie = proxies[x].replace("\n", "")
         proxy_dict = {
             'http': "http://" + proxie,
-            'https': "http://" + proxie
+            'https': "https://" + proxie
                 }
+        if windows:
+            ctypes.windll.kernel32.SetConsoleTitleW(
+                "MART by scorpion3013 | " +
+                "Proxies: " + str(len(proxies)) +
+                " | Working: " + str(len(proxy.working)) +
+                " | Bad: " + str(proxy.invalid))
         try:
             r = requests.get(proxyjudge, proxies=proxy_dict, timeout=timeout).content.decode()
             if r.__contains__(myip):
-                #print(colored(proxie + " Working but transparent", "yellow"))
                 cprint("\n" + proxie + " Working but transparent", "yellow")
+                proxy.invalid = proxy.invalid + 1
 
             else:
                 cprint("\n" + proxie + " Working", "green")
-                #print(colored("\n" + proxie + " Working", "green") + "\n")
-                working.append(proxies[x])
+                proxy.working.append(proxies[x])
         except:
             cprint("\n" + proxie + " Not Working", "red")
-            #print(colored("\n" + proxie + " Not Working", "red") + "\n")
+            proxy.invalid = proxy.invalid + 1
 
     def theads_two(numbers, threads=threads):
         pool = ThreadPool(threads)
@@ -98,5 +107,5 @@ def checkproxies(threads, timeout, proxyjudge):
         for x in range(len(proxies)):
             countt.append(int(x))
         threads_one = theads_two(countt, threads)
-    print(colored("\n\nYou have " + str(len(working)) + " working proxies.", "magenta"))
-    return working
+    print(colored("\n\nYou have " + str(len(proxy.working)) + " working proxies.", "magenta"))
+    return proxy.working
