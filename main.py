3ecea02e04ce1a41ee073acc47dbcc53822abc20
scorpion3013 <coc909scorp@gmail.com>
2018-12-20 07:42:06 +0100

    Removed 2 prints in the new check amount check

--- main.py
+++ main.py
@@ -59,14 +59,12 @@ def check(x):
         while True:
             if check_counter != Checker.check_amount:
                 answer = account_login(email_username=email_username, password=password)
-                print(answer)
                 if "errorMessage" in answer:
                     check_counter = check_counter + 1
                 else:
                     break
                     #account was valid
             else:
-                print(answer)
                 break
 
         if str(answer).__contains__("name") and str(answer.__contains__("availableProfiles")):
